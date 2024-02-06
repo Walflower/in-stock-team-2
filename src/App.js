c9961e80a029c3be35ef59eac5ac502bbbcb4de2
Indyanna Ormsby <indyanna.developer@gmail.com>
2024-02-06 13:57:45 -0800

    Took testing info out of app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.scss';
-import Testing from '../src/styles/Testing/Testing.jsx';
 
 function App() {
   return (
     <div className="App">
-      <Testing />
+      
      
     </div>
   );

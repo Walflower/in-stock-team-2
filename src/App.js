0a20aaa80c7caf5536f102677484cac30e706e98
Myriam Messaoudi <myriammess17@gmail.com>
2024-02-06 10:31:38 -0800

    created placeholder component folders

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={< />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

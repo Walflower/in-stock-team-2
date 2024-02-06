e6f5d7cea720d3c6481112f6f5bfdb4be59d12a7
Myriam Messaoudi <myriammess17@gmail.com>
2024-02-06 12:26:31 -0800

    added content to NotFound page

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 export function NotFound() {
     return (
         <div>
-            <h1>Oops! The page you are looking for does not exist</h1>
-            <p>Here are some helpful links</p>
-            <Link to='/'>Click here to go back to the Warehouses Page</Link>
+            <h1>404 - Page Not Found</h1>
+            <p>Oops! The page you are looking for does not exist</p>
+            <Link to='/'><p>Click here to go back to the Warehouses Page</p></Link>
         </div>
     )
 }

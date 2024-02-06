22c3f8cf9e29eccf563a670a4bd8cbfdf7f291c7
Indyanna Ormsby <indyanna.developer@gmail.com>
2024-02-06 10:05:56 -0800

    okay adding testing page to test the mixins, and had error in the font-family with extra quotes!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
-import './App.css';
+import './App.scss';
+import Testing from '../src/styles/Testing/Testing.jsx';
 
 function App() {
   return (
     <div className="App">
+      <Testing />
      
     </div>
   );

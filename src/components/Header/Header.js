717f539c0b4588ffee25986f180f477d1f2c8bb0
Nyajal Wal <nyajal.w@gmail.com>
2024-02-07 15:09:00 -0500

    header is looking good at mobile and desktop again

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ export function Header() {
     <header className="header">
       <nav className="header__nav">
         <img className="header__logo" src={logo} alt="instock logo" />
-        <div>
+        <div className="header__box">
           <button className="header__button">Warehouses</button>
           <button className="header__button">Inventory</button>
         </div>

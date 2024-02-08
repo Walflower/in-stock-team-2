940902b41137bc5855444a2548c320a5c4f21c0c
Nyajal Wal <nyajal.w@gmail.com>
2024-02-08 09:51:07 -0500

    completed styling for header buttons. linked pages using navlink to warehouses route and inventory route. added a hover state for both inactive and active state. added an active state.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,35 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
 import "./Header.scss";
 import logo from "../../assets/logo/InStock-Logo.svg";
 
 export function Header() {
-  //TODO: buttons are not linked to pages
+  // Get the current page path or use any other method to determine the current page
+  const currentPage = window.location.pathname;
+
+  // Function to determine if a button should have active styles
+  const isActive = (page) => currentPage === page;
   return (
     <header className="header">
       <nav className="header__nav">
         <img className="header__logo" src={logo} alt="instock logo" />
         <div className="header__box">
-          <button className="header__button">Warehouses</button>
-          <button className="header__button">Inventory</button>
+          <NavLink
+            to="/"
+            className={`header__button ${
+              isActive("/warehouses") ? "active" : ""
+            }`}
+          >
+            Warehouses
+          </NavLink>
+          <NavLink
+            to="/inventory-list"
+            className={`header__button ${
+              isActive("/inventory") ? "active" : ""
+            }`}
+          >
+            Inventory
+          </NavLink>
         </div>
       </nav>
     </header>

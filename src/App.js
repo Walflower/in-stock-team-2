83ef73723dcee5ffd04729d4da8415cf73dd740f
Myriam Messaoudi <myriammess17@gmail.com>
2024-02-08 09:12:00 -0800

    uncommented App.js

--- src/App.js
+++ src/App.js
@@ -8,30 +8,32 @@ import { Inventories } from "./pages/Inventories/Inventories";
 import InventoryList from "./components/InventoryList/InventoryList.jsx";
 function App() {
   return (
-<InventoryList/>
-    // <BrowserRouter>
-    //   <Header />
-    //   <Routes>
-    //     <Route path="/" element={<Warehouses />} />
-    //     <Route path="/:warehouseId" element={<Warehouses />} />
-    //     <Route path="/:warehouseId/edit" element={<Warehouses />} />
-    //     <Route path="/:warehouseId/delete" element={<Warehouses />} />
-    //     <Route path="/warehouse-add" element={<Warehouses />} />
-    //     <Route path="/delete-warehouse" element={<Warehouses />} />
+    <>
+{/* <InventoryList/> */}
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Warehouses />} />
+        <Route path="/:warehouseId" element={<Warehouses />} />
+        <Route path="/:warehouseId/edit" element={<Warehouses />} />
+        <Route path="/:warehouseId/delete" element={<Warehouses />} />
+        <Route path="/warehouse-add" element={<Warehouses />} />
+        <Route path="/delete-warehouse" element={<Warehouses />} />
 
-    //     <Route path="/inventory-list" element={<Inventories />} />
-    //     <Route path="/iventory-list/delete-item" element={<Inventories />} />
-    //     <Route path="/inventory-list/:iventoryId" element={<Inventories />} />
-    //     <Route
-    //       path="/inventory-list/:iventoryId/edit"
-    //       element={<Inventories />}
-    //     />
-    //     <Route path="/add-new-inventory" element={<Inventories />} />
+        <Route path="/inventory-list" element={<Inventories />} />
+        <Route path="/iventory-list/delete-item" element={<Inventories />} />
+        <Route path="/inventory-list/:iventoryId" element={<Inventories />} />
+        <Route
+          path="/inventory-list/:iventoryId/edit"
+          element={<Inventories />}
+        />
+        <Route path="/add-new-inventory" element={<Inventories />} />
 
-    //     <Route path="*" element={<NotFound />} />
-    //   </Routes>
-    //   <Footer />
-    // </BrowserRouter>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+    </>
   );
 }
 

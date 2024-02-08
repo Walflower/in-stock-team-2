7bd9ee169595ebcca55c4c694266cf99dfb3eca6
Nyajal Wal <nyajal.w@gmail.com>
2024-02-08 16:37:33 -0500

    updated the app.js to have routes to the respect components as discussed. back to orginally what Myriam did for routes feature.

--- src/App.js
+++ src/App.js
@@ -5,64 +5,48 @@ import { NotFound } from "./pages/NotFound";
 
 import { Header } from "./components/Header/Header.js";
 import { Footer } from "./components/Footer/Footer";
-import { Warehouses } from "./pages/Warehouses/Warehouses";
-import { Inventories } from "./pages/Inventories/Inventories";
 
+import { Warehouses } from "./pages/Warehouses/Warehouses";
 import { WarehouseDetails } from "./components/WarehouseDetails/WarehouseDetails";
+import { DeleteWarehouse } from "./components/DeleteWarehouse/DeleteWarehouse";
+import { AddNewWarehouse } from "./components/AddNewWarehouse/AddNewWarehouse";
+import { EditWarehouse } from "./components/EditWarehouse/EditWarehouse";
+import { EditInventory } from "./components/EditInventory/EditInventory";
+import { AddNewInventory } from "./components/AddNewInventory/AddNewInventory";
+import { InventoryList } from "./components/InventoryList/InventoryList";
+import { DeleteInventoryItem } from "./components/DeleteInventoryItem/DeleteInventoryItem";
+import { InventoryItemDetails } from "./components/InventoryItemDetails/InventoryItemDetails";
 
 function App() {
-  //DEMO
   return (
     <BrowserRouter>
       <Header />
       <main className="main">
         <div className="main__container">
           <Routes>
-            {/** part of demo */}
             <Route path="/" element={<Warehouses />} />
-            {/**example:This WarehouseDetails component would two tickets, 
-             * #25 warewhouse details and #24 the warehouse inventory list
-             * which will get the info from the api respectively.
-             * The page title/warehouse name will come from the mapping.
-             * any other components (buttons/action images/in or outof stock
-             * can be imported from the ready made style mixin*/}
-            <Route path="/:warehouseId" element={<WarehouseDetails />} /> 
-
+            <Route path="/:warehouseId" element={<WarehouseDetails />} />
+            <Route path="/:warehouseId/edit" element={<EditWarehouse />} />
             <Route
-              path="/:warehouseId/edit"
-              element={
-                <>{/**the h1 tag would be the EditWarehouse component */}
-                  <h1>edit the warehouse</h1>
-                </>
-              }
+              path="/:warehouseId/delete-warehouse"
+              element={<DeleteWarehouse />}
             />
-            <Route
-              path="/:warehouseId/delete"
-              element={
-                <>{/**the h1 tag would be the Delete warehouse component */}
-                  <h1>delete the warehouse</h1>
-                </>
-              }
-            />
-            {/** end of demo */}
+            <Route path="/warehouse-add" element={<AddNewWarehouse />} />
 
-            <Route path="/warehouse-add" element={<Warehouses />} />
-            <Route path="/delete-warehouse" element={<Warehouses />} />
-
-            <Route path="/inventory-list" element={<Inventories />} />
-            <Route
-              path="/iventory-list/delete-item"
-              element={<Inventories />}
-            />
+            <Route path="/inventory-list" element={<InventoryList />} />
             <Route
               path="/inventory-list/:iventoryId"
-              element={<Inventories />}
+              element={<InventoryItemDetails />}
+            />
+            <Route
+              path="/inventory-list/:iventoryId/delete-item"
+              element={<DeleteInventoryItem />}
             />
             <Route
               path="/inventory-list/:iventoryId/edit"
-              element={<Inventories />}
+              element={<EditInventory />}
             />
-            <Route path="/add-new-inventory" element={<Inventories />} />
+            <Route path="/inventory-list/add" element={<AddNewInventory />} />
 
             <Route path="*" element={<NotFound />} />
           </Routes>

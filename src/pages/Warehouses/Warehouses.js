0df8c75cd8700a3d32d488ba923440c9baad1fc7
Indyanna Ormsby <indyanna.developer@gmail.com>
2024-02-07 19:28:23 -0800

    forgot to remove the import for testing on warehouse

--- src/pages/Warehouses/Warehouses.js
+++ src/pages/Warehouses/Warehouses.js
@@ -1,25 +1,20 @@
 // home page, houseing the warehouse list
 import "./Warehouses.scss";
 
-import Testing from "../../styles/Testing/Testing.jsx";
-
 export function Warehouses() {
-  return (
-    <main className="main">
-      <div className="main__container">
-        <section>
-          <h1>Warehouses</h1>
-          <input></input>
-          <button></button>
-        </section>
-        <section className="main__box">section</section>
-        <section className="main__box">section</section>
-        <section className="main__box">section</section>
-        <section className="main__box">section</section>
-
-        
-
-      </div>
-    </main>
-  );
+	return (
+		<main className="main">
+			<div className="main__container">
+				<section>
+					<h1>Warehouses</h1>
+					<input></input>
+					<button></button>
+				</section>
+				<section className="main__box">section</section>
+				<section className="main__box">section</section>
+				<section className="main__box">section</section>
+				<section className="main__box">section</section>
+			</div>
+		</main>
+	);
 }

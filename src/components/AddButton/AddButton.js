3c5f741a29b6876b3ee9d5812aa5051f08950436
Myriam Messaoudi <myriammess17@gmail.com>
2024-02-08 08:48:09 -0800

    started AddButtonComponent for button on AddWarehouse component

---
+++ src/components/AddButton/AddButton.js
@@ -0,0 +1,11 @@
+import './AddButton.scss';
+
+
+
+export function AddButton() {
+    return (
+        <div className='button__container'>
+            <button className='button__add'>Add Warehouse</button>
+        </div>
+    )
+}
--- src/components/AddNewWarehouse/AddNewWarehouse.js
+++ src/components/AddNewWarehouse/AddNewWarehouse.js
@@ -1,3 +1,4 @@
+import { AddButton } from "../AddButton/AddButton"
 import "./AddNewWarehouse.scss"
 
 export function AddNewWarehouse() {
@@ -15,22 +16,22 @@ export function AddNewWarehouse() {
                         
                         <div className='details__subcontainer'>
                             <label htmlFor="location" className='details__label'>Warehouse Name</label>
-                            <input type='text' id="location" name="location" placeholder='Washington' className='details__input'></input>
+                            <input type='text' id="location" name="location" placeholder='Warehouse Name' className='details__input'></input>
                         </div>
 
                         <div className='details__subcontainer'>
                             <label htmlFor="street" className='details__label'>Street Address</label>
-                            <input type='text' id="street" name="street" placeholder='300 Pearl Street SW'  className='details__input'></input>
+                            <input type='text' id="street" name="street" placeholder='Street Address' className='details__input'></input>
                         </div>
 
                         <div className='details__subcontainer'>
                             <label htmlFor="city" className='details__label'>City</label>
-                            <input type='text' id="city" name="city" placeholder='Washington'  className='details__input'></input>
+                            <input type='text' id="city" name="city" placeholder='City'  className='details__input'></input>
                         </div>
 
                         <div className='details__subcontainer'>
                             <label htmlFor="country" className='details__label'>Country</label>
-                            <input type='text' id="country" name="country" placeholder='USA'  className='details__input'></input>
+                            <input type='text' id="country" name="country" placeholder='Country'  className='details__input'></input>
                         </div>    
 
                         </article>
@@ -43,30 +44,30 @@ export function AddNewWarehouse() {
                         
                         <div className='details__subcontainer'>
                             <label htmlFor="name" className='details__label'>Contact Name</label>
-                            <input type='text' id="name" name="name" placeholder='Graeme Lyon' className='details__input'></input>
+                            <input type='text' id="name" name="name" placeholder='Contact Name' className='details__input'></input>
                         </div>    
 
                         <div className='details__subcontainer'>
                             <label htmlFor="position" className='details__label'>Position</label>
-                            <input type='text' id="position" name="position" placeholder='Warehouse Manager' className='details__input'></input>
+                            <input type='text' id="position" name="position" placeholder='Position' className='details__input'></input>
                         </div>    
 
 
                         <div className='details__subcontainer'>
                             <label htmlFor="phone" className='details__label'>Phone Number</label>
-                            <input type='number' id="phone" name="phone" placeholder='+1 (647) 504-0911' className='details__input'></input>
+                            <input type='number' id="phone" name="phone" placeholder='Phone Number' className='details__input'></input>
                         </div>    
 
                         <div className='details__subcontainer'>
                             <label htmlFor="email" className='details__label'>Phone Number</label>
-                            <input type='email' id="email" name="email" placeholder='glyon@instock.com' className='details__input'></input>
+                            <input type='email' id="email" name="email" placeholder='Email' className='details__input'></input>
                         </div>     
 
                     </article>
                     
                         <div className='button'>
-                                <button className='button__save'>Save</button>
-                                <button className='button__cancel'>Cancel</button>
+                            <button className='button__cancel'>Cancel</button>
+                            <AddButton/>
                         </div>
 
                 </form>

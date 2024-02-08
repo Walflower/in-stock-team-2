782a328a0e3aa68446b985219e5ae91ef1a35361
Nyajal Wal <nyajal.w@gmail.com>
2024-02-08 17:08:31 -0500

    deleted test code.

--- src/components/Warehouses/Warehouses.js
+++ src/components/Warehouses/Warehouses.js
@@ -3,11 +3,5 @@ import "./Warehouses.scss";
 import React from "react";
 
 export function Warehouses() {
-  return (
-    <>
-      <h1>warehouses</h1>
-      <button>add new warehouse</button>
-      <p>list of all the warehouses</p>
-    </>
-  );
+  return <></>;
 }

9c6130a0c39ea3e024ba755a42e2f366121c93bc
Myriam Messaoudi <myriammess17@gmail.com>
2024-02-10 14:14:50 -0800

    updated get request for inventory item details to include a useState & get request for warehouse given warehouse name is linked to warehouses not inventories table

--- src/components/InventoryItemDetails/InventoryItemDetails.js
+++ src/components/InventoryItemDetails/InventoryItemDetails.js
@@ -6,28 +6,38 @@ import { useEffect } from "react";
 import edit from "../../assets/icons/edit-24px.svg";
 import { Link } from "react-router-dom";
 
-export const InventoryItemDetails = () => {
+export const InventoryItemDetails = ({inventoryId}) => {
 
   const REACT_APP_API_BASE_PATH = process.env
 
   const [item, setItem] = useState(null)
-  const inventoryId = useParams()
+  const [warehouseName, setWarehouseName] = useState(null)
 
   useEffect(() => {
-    const getItem = async () => {
+    const getItemAndWarehouse = async () => {
       try {
-        const response = await axios.get(`http://localhost8080/inventories/${inventoryId}`)
-        setItem(response.data)
-      } catch (error) {
-        console.error('Error finding inventory item you are looking for', error)
-      }
-    }
-    getItem()
-  }, [inventoryId])
+        const inventoryResponse = await axios.get(`http://localhost:8080/inventories/${inventoryId}`)
+        const inventoryData = inventoryResponse.data
+        console.log(inventoryData)
+        setItem(inventoryData)
 
-  if(!item) {
-    return <div>Loading...</div>
-  }
+        if (inventoryData && inventoryData.warehouse_id) {
+          const warehouseResponse = await axios.get(`http://localhost:8080/warehouses/${inventoryData.warehouse_id}`)
+          const warehouseData = warehouseResponse.data
+          console.log('Warehouse data: ', warehouseData)
+          setWarehouseName(warehouseData.warehouse_name)
+        }
+        
+        } catch (error) {
+          console.error('Error finding inventory item you are looking for', error)
+        }
+      }
+      getItemAndWarehouse()
+    }, [inventoryId])
+
+    if(!item || !warehouseName) {
+      return <div>Loading...</div>
+    }
   
   return (
     <>
@@ -35,8 +45,30 @@ export const InventoryItemDetails = () => {
       <Link to={`/inventory-list/${inventoryId}/edit`}><img className="" src={edit} alt="">{}</img></Link>
 
       <div className="">
-         <h2 className="">ITEM DESCRIPTION</h2>
-         <p className="">{item.description}</p>
+        <div className="">
+          <h2 className="">ITEM DESCRIPTION</h2>
+            <p className="">{item.description}</p>
+        </div>  
+        
+        <div className="">
+          <h2 className="">CATEGORY</h2>
+            <p className="">{item.category}</p>
+        </div>
+
+        <div className="">
+          <h2 className="">STATUS</h2>
+            <p className="">{item.status}</p>
+        </div>
+
+        <div className="">
+          <h2 className="">QUANTITY</h2>
+            <p className="">{item.quantity}</p>
+        </div>
+
+        <div className="">
+          <h2 className="">WAREHOUSE</h2>
+            <p className="">{warehouseName}</p>
+        </div>
 
       </div>
      

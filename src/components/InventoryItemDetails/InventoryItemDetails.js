c36a522d120db601d8abef394f9b002af4a2a83d
Myriam Messaoudi <myriammess17@gmail.com>
2024-02-11 16:09:11 -0800

    cleaned up commented out code

--- src/components/InventoryItemDetails/InventoryItemDetails.js
+++ src/components/InventoryItemDetails/InventoryItemDetails.js
@@ -7,65 +7,6 @@ import { Link, useParams } from "react-router-dom";
 
 export const InventoryItemDetails = () => {
 
-  // const REACT_APP_API_BASE_PATH = process.env
-
-  // const {inventoryId} = useParams()
-  // console.log("Inventory ID:", inventoryId)
-
-  // const [warehouseName, setWarehouseName] = useState(null)
-  // const [inventory, setInventory] = useState(null)
-
-  // useEffect(() => {
-  //   const getItemAndWarehouse = async () => {
-  //     try {
-  //       const inventoryResponse = await axios.get(`http://localhost:8080/inventories/${inventoryId}`)
-  //       const inventoryData = inventoryResponse.data
-  //       console.log(inventoryData)
-  //       setInventory(inventoryData)
-
-  //       if (inventoryData && inventoryData.warehouse_id) {
-  //         const warehouseResponse = await axios.get(`http://localhost:8080/warehouses/${inventoryData.warehouse_id}`)
-  //         const warehouseData = warehouseResponse.data
-  //         console.log('Warehouse data: ', warehouseData)
-  //         setWarehouseName(warehouseData.warehouse_name)
-  //       }
-
-  //       } catch (error) {
-  //         console.error('Error finding inventory item you are looking for', error)
-  //       }
-  //     }
-  //     getItemAndWarehouse()
-  //   }, [inventoryId])
-
-  //   if(!inventory || !warehouseName) {
-  //     return <div>Item not found...</div>
-  //   }
-  
-  // *************************************************************
-
-
-  // const { REACT_APP_API_BASE_PATH } = process.env;
-  // const {inventoryId} = useParams()
-  // console.log("Params: ", inventoryId)
-
-  // const [inventoryDetails, setInventoryDetails] = useState({})
-
-  // useEffect(() => {
-  //   const getInventoryDetails = async () => {
-  //     try {
-  //       if (inventoryId) {
-  //         const res = await axios.get(`http://localhost:8080/inventories/${inventoryId}`)
-  //         console.log("respsonse", res.data)
-  //         setInventoryDetails(res.data)
-  //       }
-  //     } catch (error) {
-  //       console.error("Error getting Inventory Item", error)
-  //     }
-  //   }
-  //   console.log("Fetching inventory detaisl from ID", inventoryDetails)
-  //   getInventoryDetails()
-  // }, [inventoryId])
-
   const { inventoryId } = useParams();
   const [inventoryDetails, setInventoryDetails] = useState(null);
 

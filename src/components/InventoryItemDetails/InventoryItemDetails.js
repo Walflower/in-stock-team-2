9d93940024c0e95fb7bab1f775250a5474a37f5f
Myriam Messaoudi <myriammess17@gmail.com>
2024-02-11 18:49:11 -0800

    updated styling and put in get for warehouse_name using the warehouse_id in the inventory items call

--- src/components/InventoryItemDetails/InventoryItemDetails.js
+++ src/components/InventoryItemDetails/InventoryItemDetails.js
@@ -4,17 +4,23 @@ import axios from "axios";
 import { useEffect } from "react";
 import edit from "../../assets/icons/edit-24px.svg";
 import { Link, useParams } from "react-router-dom";
+import returnArrow from "../../assets/icons/arrow_back-24px.svg";
 
 export const InventoryItemDetails = () => {
 
   const { inventoryId } = useParams();
   const [inventoryDetails, setInventoryDetails] = useState(null);
+  const [warehouseName, setWarehouseName] = useState(null);
 
   useEffect(() => {
     const fetchInventoryDetails = async () => {
       try {
         const response = await axios.get(`http://localhost:8080/inventories/${inventoryId}`);
         setInventoryDetails(response.data);
+
+        const warehouseResponse = await axios.get(`http://localhost:8080/warehouses/${response.data.warehouse_id}`);
+        setWarehouseName(warehouseResponse.data.warehouse_name);
+
       } catch (error) {
         console.error('Error fetching inventory details:', error);
       }
@@ -32,35 +38,45 @@ export const InventoryItemDetails = () => {
 
       {inventoryDetails && (
         <> 
-        <h1 className="">{inventoryDetails.item_name}</h1>
-        <Link to={`/inventory-list/${inventoryId}/edit`}><button><img className="" src={edit} alt="">{}</img></button></Link>
-        <div className="">
-          <div className="">
-            <h2 className="">ITEM DESCRIPTION</h2>
-              <p className="">{inventoryDetails?.description}</p>
+        <header className="header__details">
+          <div className="header__container">
+            <h1 className="header__name"><Link to="/inventory-list"><img className="header__icon--returnArrow" src={returnArrow} alt=""></img></Link>{inventoryDetails.item_name}</h1>
+            <Link to={`/inventory-list/${inventoryId}/edit`}><button className="header__button"><img className="header__icon" src={edit} alt="">{}</img></button></Link>
+          </div>
+        </header>
+
+        <section className="details">
+
+          <div className="details__container1">
+            <h2 className="details__subheader">ITEM DESCRIPTION:</h2>
+              <p className="details__description">{inventoryDetails?.description}</p>
           </div>  
           
-          <div className="">
-            <h2 className="">CATEGORY</h2>
-              <p className="">{inventoryDetails?.category}</p>
+          <div className="details__container1">
+            <h2 className="details__subheader">CATEGORY:</h2>
+              <p className="details__description">{inventoryDetails?.category}</p>
           </div>
 
-          <div className="">
-            <h2 className="">STATUS</h2>
-              <p className="">{inventoryDetails?.status}</p>
+          <div className="details__container2">
+            <div className="details__container--type">
+              <h2 className="details__subheader">STATUS:</h2>
+              <p className={`inventory__detail-tablet inventory__detail-tag-tablet 
+                    ${inventoryDetails.status.toLowerCase() === "in stock" ? "inventory__detail-tag-in" : "inventory__detail-tag-out"}`}>
+                    {inventoryDetails.status}</p>
+            </div>          
+            
+            <div className="details__container--type1">
+              <h2 className="details__subheader">QUANTITY:</h2>
+                <p className="details__description">{inventoryDetails?.quantity}</p>
+            </div>
           </div>
 
-          <div className="">
-            <h2 className="">QUANTITY</h2>
-              <p className="">{inventoryDetails?.quantity}</p>
+          <div className="details__container1">
+            <h2 className="details__subheader">WAREHOUSE:</h2>
+              <p className="details__description">{warehouseName}</p>
           </div>
 
-          <div className="">
-            <h2 className="">WAREHOUSE</h2>
-              <p className="">{inventoryDetails?.warehouse_name}</p>
-          </div>
-
-        </div>
+        </section>
     </>
       )}
     </>

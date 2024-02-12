6332d1318f22fd3668754058415f526fb2615901
Myriam Messaoudi <myriammess17@gmail.com>
2024-02-11 23:48:08 -0800

    styling

--- src/components/Warehouses/Warehouses.jsx
+++ src/components/Warehouses/Warehouses.jsx
@@ -4,6 +4,9 @@ import "./Warehouses.scss";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import chevron from "../../assets/icons/chevron_right-24px.svg";
+import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
+import editIcon from "../../assets/icons/edit-24px.svg";
 
 export function Warehouses() {
   const { REACT_APP_API_BASE_PATH } = process.env;
@@ -27,10 +30,10 @@ export function Warehouses() {
 
   return (
     <>
-      <section>
+      <section className="warehouses__header--landing">
         <h1>Warehouses</h1>
 
-        <input></input>
+        <input className="warehouses__input" placeholder="Search..."></input>
         <Link to="/warehouse-add">
               <button className="inventorylist__add">Add New Warehouse</button>
             </Link>
@@ -39,16 +42,33 @@ export function Warehouses() {
       {!loading && warehouseList && warehouseList.length > 0 && (
         <>
           {warehouseList.map((warehouse) => (
-            <div key={warehouse.id}>
+            <div className="warehouses__container--landing" key={warehouse.id}>
               <Link to={`/${warehouse.id}`}>
-                <p>{warehouse.warehouse_name}</p>
+                <p className="inventory__detail-tablet inventory__detail--link-tablet">{warehouse.warehouse_name}</p>
               </Link>
-              <p>{warehouse.address}</p>
-              <p>{warehouse.city}</p>
-              <p>{warehouse.country}</p>
-              <p>{warehouse.contact_name}</p>
-              <p>{warehouse.contact__phone}</p>
-              <p>{warehouse.contact__email}</p>
+              <div className="warehouses__subcontainer--landing">
+                <div>
+                  <p className="info__detail">{warehouse.address}</p>
+                  <p className="info__detail">{warehouse.city}</p>
+                  <p className="info__detail">{warehouse.country}</p>
+                </div>
+
+                <div>
+                  <p className="info__detail">{warehouse.contact_name}</p>
+                </div>
+
+                <div>
+                  <p className="info__detail">{warehouse.contact__phone}</p>
+                  <p className="info__detail">{warehouse.contact__email}</p>
+                </div>
+
+                <div>
+                  <img className="" src={deleteIcon} alt=""></img>
+                  <img className="" src={editIcon} alt=""></img>
+                </div>
+
+              </div>
+            
             </div>
           ))}
         </>
